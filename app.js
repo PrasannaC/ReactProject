f2db68187947e23256bb1c47eb9317e45fb4c0cf
Prasanna Choudhari <choulinux@gmail.com>
2017-04-09 09:51:05 +0530

    Updated the GET action

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ var request = require('request');
 global.combination = [];
 
 app.get('/LEDon', function(req, res) {
+    //re-init global.combination to avoid duplicate
+    global.combination = [];
 
     var url = 'http://webrates.truefx.com/rates/connect.html?f=csv';
     csv.fromStream(request(url))
       .on("data", function(data){
+        
         var finalData = [];
         for (i = 0; i< data.length; i++){
         	if (i == 0 || i == 4 || i == 5){
-        		finalData.push(data[i]);
+                if(data[i].length > 0)
+        		  finalData.push(data[i]);
         	}
         }
         global.combination.push(finalData);
-        //console.log(global.combination);
-	});
-    res.send(global.combination);
+        
+	}).on("end", function(){       // adding on end callback, to send the response only once all the records are finished reading and parsing.
+         console.log("done");
+         res.send(global.combination);
+    });;
+    
 });
 app.listen(1337);
