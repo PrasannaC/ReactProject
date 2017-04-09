db14dba51bb75e35ab9f675885b4fe8b6ea78a77
Prasanna Choudhari <choulinux@gmail.com>
2017-04-09 09:56:09 +0530

    Removed global variable
    removed global result array, as it would create issue for concurrent requests.

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var csv = require( "fast-csv" );
 var csv1 = require("csv");
 var request = require('request');
-global.combination = [];
+//global.combination = [];
 
 app.get('/LEDon', function(req, res) {
     //re-init global.combination to avoid duplicate
-    global.combination = [];
+    var combination = [];
 
     var url = 'http://webrates.truefx.com/rates/connect.html?f=csv';
     csv.fromStream(request(url))
@@ -20,11 +20,11 @@ app.get('/LEDon', function(req, res) {
         		  finalData.push(data[i]);
         	}
         }
-        global.combination.push(finalData);
+        combination.push(finalData);
         
 	}).on("end", function(){       // adding on end callback, to send the response only once all the records are finished reading and parsing.
          console.log("done");
-         res.send(global.combination);
+         res.send(combination);
     });;
     
 });
